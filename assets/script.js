f1e91c93937dd97ff05428d96515ba342d027410
seanwsutter <seanwsutter@gmail.com>
2023-09-13 10:20:02 -0500

    finished js

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,42 @@
 // var shape="circle"; 
 var topics=['HTML', 'CSS', 'Git', 'Javascript'];
-console.log(topics);
-
-
-
-// if (topic === 'HTML') {
-//  console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//  console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//  console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//  console.log("Let's study JavaScript!");
-// } else {
-//  console.log('Please try again!');
-// }
-
-
-
-
-var studentInfo = ["Sean", 32, true];
-console.log('Student Info:', studentInfo);
+// console.log(topics);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+// var studentInfo = ["Sean", 32, true];
+// console.log('Student Info:', studentInfo);
 
 // var shapes = ["triangle", "square", "pentagon", "circle"];
 // console.log(shapes[0]);
+function listTopics() {
+   for(var x=0; x<topics.length; x++) {
+      console.log(topics[x]);
+    }
+}
 
-for(var x=0; x<topics.length; x++){
-  console.log(topics[x]);
-};
+
+function selectTopic() {
+   if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+     } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+     } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+     } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+     } else {
+      console.log('Please try again!');
+     }
+
+}
+console.log('topics we learned through the prework:');
+listTopics();
+console.log('which topic should we study first?');
+selectTopic();
+//Math.floor() and Math.random() are both properties of JavaScript's built-in Math object 
+// that can be used together to generate a random number.
+
 
 /* 
-
 Conditional: If (a plate is fancy) {
    put it on the top shelf
  } else if (a plate is plain) {
